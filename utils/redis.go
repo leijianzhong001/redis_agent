9656b362923db25a10cae38f517a72b6be3a7d9d
雷建忠 <18761665514@163.com>
2022-11-13 21:42:44 +0800

    提交

--- utils/redis.go
+++ utils/redis.go
@@ -8,7 +8,7 @@ import (
 var RedisClient *redis.Client
 var lock sync.Mutex
 
-func GetRedisClient() *redis.Client{
+func GetRedisClient() *redis.Client {
 	if RedisClient != nil {
 		return RedisClient
 	}
@@ -18,7 +18,7 @@ func GetRedisClient() *redis.Client{
 	defer lock.Unlock()
 	if RedisClient == nil {
 		RedisClient = redis.NewClient(&redis.Options{
-			Addr:     "10.243.66.12:30001", //"localhost:6379"
+			Addr:     "localhost:6379", //"localhost:6379"
 			Username: "default",
 			Password: "c4b883c1cba107078b6e0eb6f5677b6a4fcf4046639f2d89a5ec43620efe6e12",
 		})
